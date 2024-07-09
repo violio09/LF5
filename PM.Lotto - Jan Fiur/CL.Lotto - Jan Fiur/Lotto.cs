b2222de78eaafee20c01070330176cd956ed9e58
Jan Maximilian Fiur <84068766+violio09@users.noreply.github.com>
2024-07-09 09:48:31 +0200

    Add Lotto 09.07
    Lotto Lists

---
+++ PM.Lotto - Jan Fiur/CL.Lotto - Jan Fiur/Lotto.cs
@@ -0,0 +1,20 @@
+﻿namespace CL.Lotto___Jan_Fiur
+{
+    public class Lotto
+    {
+        private List<int> lottoZahlen = new List<int>();
+
+        public List<int> GetList()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                int ziehZahl = random.Next(0,50);
+                lottoZahlen.Add(ziehZahl);
+            }
+
+            return lottoZahlen;
+        }
+    }
+}
---
+++ PM.Lotto - Jan Fiur/Lotto - Jan Fiur/Program.cs
@@ -0,0 +1,50 @@
+﻿using CL.Lotto___Jan_Fiur;
+
+Lotto ziehZahlen = new Lotto();
+
+List<int> userZahlen = new List<int>();
+
+int richtigeZahlen = 0;
+int falscheZahlen = 0;
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine("Bitte geben sie ihre Tippzahl ein.");
+
+
+    userZahlen.Add(Convert.ToInt32(Console.ReadLine()));
+
+    Console.Clear();
+}
+
+List<int> calledList = ziehZahlen.GetList();
+
+foreach (int called in calledList)
+{
+    Console.Write("|" + called + "|");
+}
+
+Console.WriteLine("\n");
+
+foreach (int nutzer in userZahlen)
+{
+    Console.Write("|" + nutzer + "|");
+}
+
+for(int i = 0;i < 6; i++)
+{
+    if (calledList[i] == userZahlen[i])
+    {
+        richtigeZahlen++;
+    }
+    else{ 
+
+    falscheZahlen++; 
+
+    }
+}
+Console.WriteLine("\n");
+Console.WriteLine("Die Überprüfung hatte folgendes ergeben: \n");
+Console.WriteLine("Sie hatten " + richtigeZahlen + " richtige Zahlen und " + falscheZahlen + " falsche Zahlen");
+
+
