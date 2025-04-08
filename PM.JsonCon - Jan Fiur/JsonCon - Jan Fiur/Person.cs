a6b0ac158b9826a31707bd69f9ff94a8e5d7a96b
Jan Maximilian Fiur <84068766+violio09@users.noreply.github.com>
2025-04-08 11:05:09 +0200

    Json
    json

---
+++ PM.JsonCon - Jan Fiur/JsonCon - Jan Fiur/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonCon___Jan_Fiur
+{
+    public class Person
+    {
+        public string vorname { get; set; }
+
+        public string nachname { get; set; }
+
+        public int alter {  get; set; } 
+
+        public string beruf {  get; set; }
+
+        public Person(string vorname, string nachname, int alter, string beruf) 
+        {
+            this.vorname = vorname;
+            this.nachname = nachname;
+            this.alter = alter;
+            this.beruf = beruf;
+        }
+    }
+}
---
+++ PM.JsonCon - Jan Fiur/JsonCon - Jan Fiur/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using System.Xml;
+using JsonCon___Jan_Fiur;
+
+
+string json = File.ReadAllText("C:/LF5/PM.JsonCon - Jan Fiur/JsonCon - Jan Fiur/jsonFiles/personen.json");
+
+Person[] personenArray = JsonSerializer.Deserialize<Person[]>(json);
+
+for (int i = 0; i < personenArray.Length; i++)
+{
+    Person person = personenArray[i];
+
+    if (person.vorname == "Ben" && person.nachname == "Schulz")
+    {
+        person.alter = 26;
+    }
+
+    Console.WriteLine($"Vorname: {person.vorname}");
+    Console.WriteLine($"Nachname: {person.nachname}");
+    Console.WriteLine($"Alter: {person.alter}");
+    Console.WriteLine($"Beruf: {person.beruf} \n");
+
+    string jsonString = JsonSerializer.Serialize(personenArray[i]);
+
+    File.AppendAllLines("C:/LF5/PM.JsonCon - Jan Fiur/JsonCon - Jan Fiur/jsonFiles/neuePersonen.json", [jsonString]);
+}
+
+
+
